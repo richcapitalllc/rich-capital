78a4ae0f0524337291f6c2f6f6bcc12d69f65f42
richcapitalllc <richcapitalllc@gmail.com>
2025-07-14 14:34:16 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to Rich Capital LLC</h1>
+      <p className="mt-4 text-lg">Your future, financed smartly.</p>
+    </div>
+  );
+}
+
+export default App;
